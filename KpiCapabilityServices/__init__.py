e92fb378410176312a216feb486f9cf182d2b8e0
eatakahraman <eatakahraman@gmail.com>
2020-01-23 17:00:06 -0600

    Deneme

--- KpiCapabilityServices/__init__.py
+++ KpiCapabilityServices/__init__.py
@@ -1,3 +1,8 @@
+import pandas as pd
+from BaseServices import BaseKPI
 
-
+sql = 'SELECT * FROM HPS_METRICS.dbo.Dim_KPI'
+conn = BaseKPI.dbConnection()
+data = pd.read_sql(sql, conn)
+print(data.head)
 
