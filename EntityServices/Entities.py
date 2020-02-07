d67e69ccbbfc9f55daf4cb54092467967dca37d4
eatakahraman <eatakahraman@gmail.com>
2020-02-07 09:13:27 -0600

    First commit: DB Select and Insert methods

--- EntityServices/Entities.py
+++ EntityServices/Entities.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from BaseServices import BaseKPI
 
-class Entities():
+class KpiOperations():
     global conn
     conn = BaseKPI.dbConnection()
 
@@ -13,3 +13,8 @@ class Entities():
         sql='select k.RowID KPI_Row_Id,c.CategoryKey,w.Term_RowID,d.DepartmentKey,w.Is_KPI_Applicable,w.Weight from Dim_kpi k join Dim_Category C on  c.CategoryKey=k.CategoryKey join dim_department d on d.DepartmentKey=c.DepartmentKey join.dim_kpi_weight w on w.KPI_RowID=k.RowID where k.RowID={}'.format(kpiRowID)
         kpiDetails = pd.read_sql(sql, conn)
         return kpiDetails
+
+    def getDistrictsForAllCampuses():
+        sql='SELECT District_RowID,CampusKey FROM [HPS_METRICS_QA].[dbo].[Dim_Campus]'
+        return pd.read_sql(sql, conn)
+
--- KpiCapabilityServices/__init__.py
+++ KpiCapabilityServices/__init__.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from BaseServices import BaseKPI
 
-sql = 'SELECT * FROM HPS_METRICS.dbo.Dim_KPI'
+sql = 'SELECT * FROM HPS_METRICS_QA.dbo.Dim_KPI'
 conn = BaseKPI.dbConnection()
 data = pd.read_sql(sql, conn)
 print(data.head)
