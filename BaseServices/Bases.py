29d6662b993030a0c282e6c3798b9678631bb12b
eatakahraman <eatakahraman@gmail.com>
2020-02-26 12:03:17 -0600

    KPIs

--- BaseServices/Bases.py
+++ BaseServices/Bases.py
@@ -29,9 +29,8 @@ class BaseKPI():
         districts = Entities.KpiOperations.getDistrictsForAllCampuses()
         df = pd.merge(df, districts, left_on='Campus_RowID', right_on='CampusKey')
         df = df[['District_RowID', 'Campus_RowID', 'Term_RowID', 'KPI_RowID', 'Category_RowID',
-                 'Department_RowID', 'Is_KPI_Applicable', 'Adjusted_Weight', 'Adjusted_Score', 'Score', 'Raw_Score']]
-        df['Raw_Score_Details'] = 'CustomDev.KPI_60400090001'
-        df['Artifact_URL'] = 'External Reports'
+                 'Department_RowID', 'Is_KPI_Applicable', 'Adjusted_Weight', 'Adjusted_Score', 'Score', 'Raw_Score', 'Raw_Score_Details', 'Artifact_URL']]
+
         # Delete old rows(if exists) for this KPI for this specific term before inserting new records
         Entities.KpiOperations.delKPIOldRecords(kpi_rowid, Term_RowID)
         Entities.KpiOperations.insertFactKPICampuses(df, 'Fact_KPI_Campus')
