614a7f75a4ec291e2d7dbb6061d206d90681e1cf
ibrahim Ulusoy <ibrahimulusoy@yahoo.com>
2020-05-06 18:09:49 -0500

    .

---
+++ KpiCapabilityServices/KPI_80200050001.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from BaseServices import Bases
+
+
+df = pd.read_csv(r"C:\Users\iulusoy\Desktop\KPI\KPI_80200050001.csv")
+
+df["Raw_Score"] = (df["EBRW_Average"])
+df['Raw_Score_Details'] = 'CustomDev.KPI_80200050001'
+df['Artifact_URL'] = 'SKYWARD'
+
+print(df["Raw_Score"])
+
+Bases.BaseKPI.setKPIDetails(df, True, 80200050001)
--- SeleniumServices/TEA_Download.py
+++ SeleniumServices/TEA_Download.py
@@ -2,7 +2,12 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 
-driver=webdriver.Chrome(ChromeDriverManager().install())
+options = webdriver.ChromeOptions()
+prefs = {'download.default_directory':'C:\Downloads'}
+options.add_experimental_option('prefs', prefs)
+driver=webdriver.Chrome(ChromeDriverManager().install(),chrome_options=options)
+
+#Donload district rates
 driver.get("https://rptsvr1.tea.texas.gov/perfreport/account/2019/download.html")
 driver.maximize_window()
 driver.implicitly_wait(10)
@@ -13,7 +18,21 @@ time.sleep(2)
 driver.find_element_by_name("selall").click()
 time.sleep(2)
 driver.find_element_by_css_selector("input[type='submit'][value='Download']").click()
+time.sleep(5)
+#Donload district rates
 
+#Donload district rates
+driver.get("https://rptsvr1.tea.texas.gov/perfreport/account/2019/download.html")
+time.sleep(2)
+driver.find_element_by_css_selector("input[type='radio'][value='C']").click()
+#WebDriverWait(driver, 10).until(EC.presence_of_element_located(By.CSS_SELECTOR, "body > div > div.container.pt-5.flex-grow-1.flex-shrink-0.d-flex.flex-column > div.row.mt-auto.text-center.text-sm-left > div > p > a")
+driver.find_element_by_css_selector("input[type='submit'][value='Continue']").click()
+time.sleep(2)
+driver.find_element_by_name("selall").click()
+time.sleep(2)
+driver.find_element_by_css_selector("input[type='submit'][value='Download']").click()
+time.sleep(2)
+driver.quit()
 
 
 
