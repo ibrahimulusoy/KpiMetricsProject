ba914fab357d17e16c24512d5e7fb7b00621b25e
ibrahim Ulusoy <ibrahimulusoy@yahoo.com>
2020-11-02 09:53:39 -0600

    .

--- KpiCapabilityServices/KPI_80200050001.py
+++ KpiCapabilityServices/KPI_80200050001.py
@@ -2,7 +2,7 @@ import pandas as pd
 from BaseServices import Bases
 
 
-df = pd.read_csv(r"C:\Users\iulusoy\Desktop\KPI\KPI_80200050001.csv")
+df = pd.read_csv(r"C:\Users\iulusoy\Desktop\KPI\HPSFS.xlsx")
 
 df["Raw_Score"] = (df["EBRW_Average"])
 df['Raw_Score_Details'] = 'CustomDev.KPI_80200050001'
