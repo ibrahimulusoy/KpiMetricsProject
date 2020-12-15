8122565a3cfaff9355b6c255f5e18385f80c3b3f
ibrahim Ulusoy <ibrahimulusoy@yahoo.com>
2020-12-14 18:57:35 -0600

    .

--- KpiCapabilityServices/KPI_60200010001.py
+++ KpiCapabilityServices/KPI_60200010001.py
@@ -6,9 +6,11 @@
 import pandas as pd
 from BaseServices import Bases
 
-df = pd.read_csv(r'{}\KPI_60200010001.csv'.format(Bases.BaseKPI.source_files_path))
+df = pd.read_csv(r'C:\Users\iulusoy\Documents\KPI\KPI_60200010001.csv'.format(Bases.BaseKPI.source_files_path))
 
 df["Raw_Score"] = (df["TotalADA"] / df["TotalEnrollmentDays"]) * 100
+df["Raw_Score"] =df["Raw_Score"].round(4)
 df["Raw_Score_Details"] = "CustomDev.KPI_60200010001"
-df["Artifact_Url"] = "SKYWARD"
+df["Artifact_URL"] = "SKYWARD"
+
 Bases.BaseKPI.setKPIDetails(df, True, 60200010001, True)
